5ed2b28bb1f02392c296065393e86ed32e25acdf
Aldegid <aldegid@gmail.com>
2019-04-15 17:27:08 +0300

    add: change design remove commented lines

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-import './App.css';
-import {getList} from './api'
-import UserCards from './components/UserCards/UserCards'
-import SearchFilter from './components/SearchFilter/SearchFilter'
-// import Sort from './components/Sort/Sort'
-import 'bootstrap/dist/css/bootstrap.css';
+import React, { Component } from "react";
+import "./App.sass";
+import { getList } from "./api";
+import UserCards from "./components/UserCards/UserCards";
+import SearchFilter from "./components/SearchFilter/SearchFilter";
+//import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   constructor(props) {
@@ -13,127 +12,142 @@ class App extends Component {
       list: null,
       data: null,
       isLoaded: false,
-      // name: null,
-      // change: null,
-      // asc: false,
-      // desc: false,
-      // alien: false,
-      // human: false,
-      currentApiUrl: 'https://rickandmortyapi.com/api/character',
-      nextApiUrl: '',
-      prevApiData: '',
-
+      name: null,
+      change: null,
+      asc: false,
+      desc: false,
+      alien: false,
+      human: false,
+      currentApiUrl: "https://rickandmortyapi.com/api/character",
+      nextApiUrl: "",
+      prevApiData: ""
     };
   }
 
   handleClickNext = () => {
-    const nextList = getList(this.state.nextApiUrl);
-    nextList.then( data => {
+    getList(this.state.nextApiUrl).then(data => {
       this.setState({
         list: data.results,
         data: data,
         nextApiUrl: data.info.next,
         prevApiData: data.info.prev
-      })
-    })
-  }
+      });
+    });
+  };
   handleClickPrev = () => {
-    const prevList = getList(this.state.prevApiData);
-    prevList.then( data => {
+    getList(this.state.prevApiData).then(data => {
       this.setState({
         list: data.results,
         data: data,
         nextApiUrl: data.info.next,
         prevApiData: data.info.prev
-      })
-    })
-  }
+      });
+    });
+  };
 
   async componentDidMount() {
     const list = await getList(this.state.currentApiUrl);
-    //console.log(list)
     this.setState({
       list: list.results,
       data: list,
       nextApiUrl: list.info.next,
       isLoaded: true
-    })
+    });
   }
 
-  performSearch = ({name,  change }) => {
-    this.setState({ name, change })
-  }
+  performSearch = ({ name, change }) => {
+    this.setState({ name, change });
+  };
   performSort = ({ asc, desc }) => {
-    this.setState({ asc, desc })
-  }
+    this.setState({ asc, desc });
+  };
   performFilter = ({ alien, human }) => {
-    this.setState({ alien, human })
-  }
+    this.setState({ alien, human });
+  };
+
+  sortByAsc = list => list.sort((a, b) => (a.name < b.name ? -1 : 1));
+
+  sortByDesc = list => list.sort((a, b) => (a.name < b.name ? 1 : -1));
+
+  filterByName = (list, name) => list.filter(item => item.name.includes(name));
+
+  filterBySpecies = (list, species) =>
+    list.filter(item => item.species === species);
 
   render() {
-
-    const {list, isLoaded, name, change, asc, desc, alien, human} = this.state;
+    const {
+      list,
+      isLoaded,
+      name,
+      change,
+      asc,
+      desc,
+      alien,
+      human
+    } = this.state;
     let result = list;
 
-    if(asc) {
-      result = list.sort((a, b) => a.name < b.name ? -1 : 1);
+    if (asc) {
+      result = this.sortByAsc(list);
     }
-    if(desc) {
-      result = list.sort((a, b) => a.name < b.name ? 1 : -1);
+    if (desc) {
+      result = this.sortByDesc(list);
     }
-    if(human) {
-      result = list.filter(item => item.species === 'Human');
+    if (human) {
+      result = this.filterBySpecies(list, "Human");
     }
-    if(alien) {
-      result = list.filter(item => item.species === 'Alien');
+    if (alien) {
+      result = this.filterBySpecies(list, "Alien");
     }
-    if(name ||change) {
-      result = list.filter(item => item.name.includes(name || change))
+    if (change) {
+      result = this.filterByName(list, change);
     }
-    //console.log('result', result);
+    if (name) {
+      result = this.filterByName(list, name);
+    }
 
-
-    if(!isLoaded) {
+    if (!isLoaded) {
       return (
-        <div className="spinner-border" role="status">
-          <span className="sr-only">Loading...</span>
+        <div className="preloader">
+          <div className="pulse" />
         </div>
-      )
-    } else {
-        return (
-          <div className="container">
-            <div className="row">
-              <div className="col-12 text-center">
-                <button
-                  className="btn btn-secondary m-2"
-                  onClick={this.handleClickPrev}
-                  disabled={!this.state.prevApiData}
-                  >
-                  ← Prev Page
-                </button>
-                <button
-                  className="btn btn-secondary m-2"
-                  onClick={this.handleClickNext}
-                  disabled={!this.state.nextApiUrl}
-                  >
-                  Next Page →
-                </button>
-                <SearchFilter
-                  handleSearch={this.performSearch}
-                  handleSort={this.performSort}
-                  handleFilter={this.performFilter}
-                />
-              </div>
-                {result.map(item => {
-                  return <UserCards key={item.id} {...item}/>
-                })}
-              </div>
-
-
-          </div>
-        );
+      );
     }
+    return (
+      <div className="container">
+        <section className="main-section">
+          <aside className="filter">
+          <div className="buttons-group">
+            <button
+                className="button"
+                onClick={this.handleClickPrev}
+                disabled={!this.state.prevApiData}
+              >
+                ← Prev Page
+              </button>
+              <button
+                className="button"
+                onClick={this.handleClickNext}
+                disabled={!this.state.nextApiUrl}
+              >
+                Next Page →
+              </button>
+          </div>
 
+            <SearchFilter
+              handleSearch={this.performSearch}
+              handleSort={this.performSort}
+              handleFilter={this.performFilter}
+            />
+          </aside>
+          <div className="users">
+            {result.map(item => {
+              return <UserCards key={item.id} {...item} />;
+            })}
+          </div>
+        </section>
+      </div>
+    );
   }
 }
 
--- src/components/SearchFilter/SearchFilter.js
+++ src/components/SearchFilter/SearchFilter.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./searchfilter.css";
 
 const SearchFilter = ({ handleSort, handleSearch, handleFilter }) => {
-  //console.log(handleChange, handleSearch)
-
   const onSubmit = e => {
     e.preventDefault();
     const { elements } = e.target;
@@ -15,11 +13,10 @@ const SearchFilter = ({ handleSort, handleSearch, handleFilter }) => {
       return acc;
     }, {});
     handleSearch(value);
-    //console.log(value);
   };
   const onChange = e => {
     const target = e.target;
-    const value = target.name === 'change' ? target.value : ''
+    const value = target.name === "change" ? target.value : "";
 
     handleSearch({
       [target.name]: value
@@ -28,10 +25,10 @@ const SearchFilter = ({ handleSort, handleSearch, handleFilter }) => {
   const onSort = e => {
     const target = e.target;
     const radioState = () => {
-      if(target.checked && target.type === 'radio') {
-        return true
+      if (target.checked && target.type === "radio") {
+        return true;
       }
-    }
+    };
     handleSort({
       [target.value]: radioState()
     });
@@ -39,102 +36,99 @@ const SearchFilter = ({ handleSort, handleSearch, handleFilter }) => {
   const onFilter = e => {
     const target = e.target;
     const radioState = () => {
-      if(target.checked && target.type === 'radio') {
-        return true
+      if (target.checked && target.type === "radio") {
+        return true;
       }
-    }
+    };
     handleFilter({
       [target.value]: radioState()
     });
   };
 
   return (
-    <form onSubmit={onSubmit} className="form">
-      <div className="sort-asc-desc d-inline-flex ml-2 mr-2">
-        <div className="form-check form-check-inline m-2">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id="inlineRadio1"
-            value="asc"
-            onChange={onSort}
-          />
-          <label className="form-check-label" htmlFor="inlineRadio1">
-            Sort by Asc
-          </label>
-        </div>
-        <div className="form-check form-check-inline m-2">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="inlineRadioOptions"
-            id="inlineRadio2"
-            value="desc"
-            onChange={onSort}
-          />
-          <label className="form-check-label" htmlFor="inlineRadio2">
-            Sort by Desc
-          </label>
-        </div>
-      </div>
-      <div className="sort-asc-desc d-inline-flex ml-2 mr-2">
-        <div className="form-check form-check-inline m-2">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="inlineRadioFilter"
-            id="inlineRadio3"
-            value="human"
-            onChange={onFilter}
-          />
-          <label className="form-check-label" htmlFor="inlineRadio3">
-            Filter by Human
-          </label>
-        </div>
-        <div className="form-check form-check-inline m-2">
-          <input
-            className="form-check-input"
-            type="radio"
-            name="inlineRadioFilter"
-            id="inlineRadio4"
-            value="alien"
-            onChange={onFilter}
-          />
-          <label className="form-check-label" htmlFor="inlineRadio4">
-            Filter by Alien
-          </label>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-6">
-          <div className="input-group mb-2">
+    <form onSubmit={onSubmit} className="form__main">
+      <div className="radio-buttons">
+        <div className="sort-asc-desc">
+          <div className="form-check">
             <input
-              type="text"
-              className="form-control"
-              name="name"
-              placeholder="onSubmit filter by name... "
-              aria-label="Recipient's username"
-              aria-describedby="button-addon2"
+              className="form-check-input"
+              type="radio"
+              name="inlineRadioOptions"
+              id="inlineRadio1"
+              value="asc"
+              onChange={onSort}
             />
-            <div className="input-group-append">
-              <button className="btn btn-outline-secondary">Button</button>
-            </div>
+            <label className="form-check-label" htmlFor="inlineRadio1">
+              Sort by Asc
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="inlineRadioOptions"
+              id="inlineRadio2"
+              value="desc"
+              onChange={onSort}
+            />
+            <label className="form-check-label" htmlFor="inlineRadio2">
+              Sort by Desc
+            </label>
           </div>
         </div>
-        <div className="col-md-6">
-          <div className="input-group mb-2">
+        <div className="sort-asc-desc">
+          <div className="form-check">
             <input
-              type="text"
-              className="form-control"
-              name="change"
-              placeholder="onChange filter by name... "
-              aria-label="Recipient's username"
-              aria-describedby="button-addon2"
-              onChange={onChange}
+              className="form-check-input"
+              type="radio"
+              name="inlineRadioFilter"
+              id="inlineRadio3"
+              value="human"
+              onChange={onFilter}
             />
+            <label className="form-check-label" htmlFor="inlineRadio3">
+              Filter by Human
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="radio"
+              name="inlineRadioFilter"
+              id="inlineRadio4"
+              value="alien"
+              onChange={onFilter}
+            />
+            <label className="form-check-label" htmlFor="inlineRadio4">
+              Filter by Alien
+            </label>
           </div>
         </div>
+      </div>
+
+      <div className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          name="name"
+          placeholder="onSubmit filter by name... "
+          aria-label="Recipient's username"
+          aria-describedby="button-addon2"
+        />
+        <div className="input-group">
+          <button className="button">Button</button>
+        </div>
+      </div>
+      <div className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          name="change"
+          placeholder="onChange filter by name... "
+          aria-label="Recipient's username"
+          aria-describedby="button-addon2"
+          onChange={onChange}
+        />
       </div>
     </form>
   );
